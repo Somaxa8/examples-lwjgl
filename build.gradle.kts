df3358cb2e8a2f26b2cd5c5c0a75d778b314cd07
somaxa8 <silviofrancoxa8@gmail.com>
2022-10-12 19:24:56 -0500

    opengl square

---
+++ build.gradle.kts
@@ -0,0 +1,31 @@
+plugins {
+    id("org.jetbrains.kotlin.jvm") version "1.6.21"
+
+    application
+}
+
+val lwjglVersion = "3.3.1"
+val jomlVersion = "1.10.5"
+val lwjglNatives = "natives-linux"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(platform("org.lwjgl:lwjgl-bom:$lwjglVersion"))
+
+    implementation("org.lwjgl", "lwjgl")
+    implementation("org.lwjgl", "lwjgl-assimp")
+    implementation("org.lwjgl", "lwjgl-glfw")
+    implementation("org.lwjgl", "lwjgl-openal")
+    implementation("org.lwjgl", "lwjgl-opengl")
+    implementation("org.lwjgl", "lwjgl-stb")
+    runtimeOnly("org.lwjgl", "lwjgl", classifier = lwjglNatives)
+    runtimeOnly("org.lwjgl", "lwjgl-assimp", classifier = lwjglNatives)
+    runtimeOnly("org.lwjgl", "lwjgl-glfw", classifier = lwjglNatives)
+    runtimeOnly("org.lwjgl", "lwjgl-openal", classifier = lwjglNatives)
+    runtimeOnly("org.lwjgl", "lwjgl-opengl", classifier = lwjglNatives)
+    runtimeOnly("org.lwjgl", "lwjgl-stb", classifier = lwjglNatives)
+    implementation("org.joml", "joml", jomlVersion)
+}
